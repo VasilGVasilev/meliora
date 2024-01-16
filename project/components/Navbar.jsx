b8c9673be638b9869c67fc2285fd9ec23461596b
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2024-01-16 19:34:07 +0200

    a links

--- project/components/Navbar.jsx
+++ project/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import { useRef, useState } from "react";
 import useMediaQuery from "../hooks/useMediaQuery"
 
-import { Link } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 import { motion } from "framer-motion"
 
@@ -96,6 +95,7 @@ export const Navbar = () => {
 
                 <div className="flex items-center justify-between mx-auto w-5/6">
                     <div></div>
+                    <HashLink smooth to="#sadasd">asd</HashLink>
                     {/* <Link className='saturate-200 duration-200 flex justify-center items-center h-14 w-20 sm:h-20 sm:w-32 ' to={'/'}>
                         <div className="relative rounded-full overflow-hidden h-10 w-14 sm:h-16 sm:w-24 shadow-lg hover:scale-110 transition duration-200">
 
