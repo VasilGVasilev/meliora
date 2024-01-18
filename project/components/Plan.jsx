2e2554c6cb7a666984f4a443bf33fb98d3950a03
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2024-01-18 22:29:27 +0200

    description some padding

--- project/components/Plan.jsx
+++ project/components/Plan.jsx
@@ -1,6 +1,6 @@
 export const Plan = () => {
     return (
-        <section id="gallery" className="bg-white">
+        <section id="plan" className="bg-white">
             <div className="flex flex-col gap-5 justify-center items-start">
                 <div className="font-playfairDisplay text-black mx-10 mt-10 text-3xl md:text-7xl relative before:absolute before:top-[30%] before:left-[10%] before:w-full before:h-full before:border-r-2 before:border-b-2 before:border-yellow-500 before:z-10">Разпределение</div>
                 <div className="p-10 h-full w-full text-black">
